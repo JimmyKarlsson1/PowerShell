ca15a6d886a0e444c43cdf20d4776b6db56a7378
JimmyKarlsson1 <jimmy.r.carlsson@gmail.com>
2019-03-04 09:59:08 +0100

    Update Enable-DockerWindows10.ps1

--- Docker/Enable-DockerWindows10.ps1
+++ Docker/Enable-DockerWindows10.ps1
@@ -8,7 +8,7 @@ Start-Process Powershell -verb Runas
 Enable-WindowsOptionalFeature -Online -FeatureName "Microsoft-Hyper-V" -All
 Enable-WindowsOptionalFeature -Online -FeatureName "Containers"
 Install-Module -Name DockerMsftProvider -Repository PSGallery -Force
-Install-Package -Name docker-desktop  -ProviderName Chocolatey
+Install-Package -Name docker-desktop  -ProviderName Chocolatey -Force -Confirm:$false
 Restart-Computer -Force
 #Login DockerID (nessesary?)
 #Linux/Windowscontainer /Tray

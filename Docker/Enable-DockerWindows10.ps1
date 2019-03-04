c7a27b7151f5da978bdaf2d2a7ae4e7e31c849df
JimmyKarlsson1 <jimmy.r.carlsson@gmail.com>
2019-03-04 09:59:28 +0100

    Update Enable-DockerWindows10.ps1

--- Docker/Enable-DockerWindows10.ps1
+++ Docker/Enable-DockerWindows10.ps1
@@ -4,7 +4,7 @@ If(!((Get-WmiObject Win32_Processor).VirtualizationFirmwareEnabled))
     exit
 }
 
-Start-Process Powershell -verb Runas
+#Start-Process Powershell -verb Runas
 Enable-WindowsOptionalFeature -Online -FeatureName "Microsoft-Hyper-V" -All
 Enable-WindowsOptionalFeature -Online -FeatureName "Containers"
 Install-Module -Name DockerMsftProvider -Repository PSGallery -Force
